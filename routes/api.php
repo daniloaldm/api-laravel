ee96fbcf06e6735dc1d3e283042777805aebf24a
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2020-08-31 21:24:01 -0300

    rota doc swagger

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// http://localhost/api/documentation
 Route::prefix('/v1')->group(function () {
     Route::get('posts', 'PostsController@show');
     Route::get('posts/{id}', 'PostsController@find');
